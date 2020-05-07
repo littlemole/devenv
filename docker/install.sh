b5e75c2e35653245c9941a0db13cbb7f1387e6ad
mike <littlemole@oha7.org>
2020-05-07 21:52:36 +0200

    ng

--- docker/install.sh
+++ docker/install.sh
@@ -4,4 +4,8 @@ set -e
 cd /usr/local/src/
 git clone https://github.com/littlemole/$1.git
 
+if [ "$BRANCH" == "ng"  ]; then
+
+	cd $1 && git checkout ng
+fi
 SKIPTESTS="true" /usr/local/bin/build.sh $1
